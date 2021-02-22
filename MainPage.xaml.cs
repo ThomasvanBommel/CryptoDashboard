f5ed2e374eadbbd3a282b559c4c6f6812f503924
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-02-22 10:39:43 -0400

    Added partial keycheck function

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,9 +39,46 @@ namespace CryptoDashboard
 
         // Unlock the rest of the application when the user submits a valid API key
         private void UnlockContent(object sender, RoutedEventArgs e) {
+            // Get key from UI
             string key = APIKey.Text;
 
+            // Check if key is empty
+            if (key != "") {
+                // Send request
+                request(key);
+            } else {
+                // User submitted empty key
+                new MessageDialog("Empty submission. Try again.").ShowAsync();
+            }
+        }
 
+        // Request a response from the Nomic API
+        private async void request(string key) {
+            // Create an HTTP client object
+            HttpClient client = new HttpClient();
+
+            // Add a user-agent header to the GET request. 
+            var headers = client.DefaultRequestHeaders;
+
+            // Nomic endpoint
+            Uri uri = new Uri("https://api.nomics.com/v1/currencies/ticker?id=BTC&per-page=1&key=" + key);
+
+            // Object that will receive data asynchronously
+            HttpResponseMessage response;
+            string json;
+
+            try {
+                // Get response
+                response = await client.GetAsync(uri);
+
+                // Ensure success
+                response.EnsureSuccessStatusCode();
+
+                // Read data
+                json = await response.Content.ReadAsStringAsync();
+            } catch {
+                new MessageDialog("Invalid API key").ShowAsync();
+            }
         }
     }
 }
